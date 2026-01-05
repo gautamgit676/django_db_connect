968e012e90f0806a103b50b2628c39a07fadfdaa
gautamgit676 <gautamgit244@gmail.com>
2026-01-05 15:55:13 +0530

    add db

--- prodbs/settings_rail.py
+++ prodbs/settings_rail.py
@@ -11,7 +11,23 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 import dj_database_url
 
-DATABASES = {
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # take environment variables from .env.
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": config("ENGINE"),
+#         "NAME": config("DB_NAME", default="railway"),
+#         "USER": config("DB_USER", default="postgres"),
+#         "PASSWORD": config("DB_PASSWORD", default="zEAQTolZLLCzIcPyGDItQZcJnvdFVOml"),
+#         "HOST": config("DB_HOST", default="postgres.railway.internal"),
+#         "PORT": config("DB_PORT", default="5432"),
+#     }
+# }
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
         "default": dj_database_url.config(
             conn_max_age=600,
             ssl_require=True,
