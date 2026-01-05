fc1ad94fb85e2d98c27260cf63540f422d2cc200
gautamgit676 <gautamgit244@gmail.com>
2026-01-05 15:11:18 +0530

    add new code

--- prodbs/settings_rail.py
+++ prodbs/settings_rail.py
@@ -2,3 +2,9 @@ from prodbs.settings import *
 from decouple import config
 
 SECRET_KEY = config("SECRET_KEY")
+ALLOWED_HOSTS = ["djangodbconnect-production.up.railway.app"]
+# ALLOWED_HOSTS = ["djangodbconnect-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['https://djangodbconnect-production.up.railway.app']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
